fdb22349c2af6f09c42d0a153c8e261f9e174b75
marvin-zapeta <marvinzapeta309@gmail.com>
2023-01-06 08:44:25 -0600

    6ENERO

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,18 +2,28 @@
 
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
-        
+        pagina: 'Iniciar Sesion'        
     })
 }
 
 const formularioRegistro = (req, res) => {
     res.render('auth/registro', {
+        pagina: 'Crear Cuenta'
+        
+    })
+}
+
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/olvide-password', {
+        pagina: 'Recupera tu accesos a Bienes Raices'
         
     })
 }
 
 
+
 export {
     formularioLogin,
-    formularioRegistro
+    formularioRegistro,
+    formularioOlvidePassword
 }
